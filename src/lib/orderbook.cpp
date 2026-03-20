084d600af8083efbc57781f388dd746a48b8d7da
wmarcos15 <guillermoml9031@gmail.com>
2026-03-20 23:08:38 +0100

    fix: better handling of adding an order with quantity of 0 now throws an exception

--- src/lib/orderbook.cpp
+++ src/lib/orderbook.cpp
@@ -84,6 +84,9 @@ bool OrderBook::canFullyFill(OrderPointer order) const {
 }
 
 OrderResult OrderBook::addOrder(OrderType type, Side side, Price price, Quantity qty) {
+    if (qty == 0)
+        throw std::invalid_argument("quantity must be greater than zero");
+
     OrderID id = nextID_++;
     OrderPointer incoming = std::make_shared<Order>(id, type, side, price, qty);
 
